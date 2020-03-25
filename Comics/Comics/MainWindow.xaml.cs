e0b281f71d3986c4d200717db16993fe4bea32f2
AnastasiaTerekhova <delta.whell@gmail.com>
2020-03-26 00:58:24 +0300

    first commit

---
+++ Comics/Comics/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Accord.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Size = System.Windows.Size;
+using Microsoft.Win32;
+
+namespace Comics
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+   
+
+    public partial class MainWindow : Window
+    {
+        VideoFileReader reader = new VideoFileReader();
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        private string _filePath;
+        private string FileName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+           
+        }
+
+            private void timerTick(object sender, object e)
+        {
+            time.Text = media1.Position.ToString(@"mm\:ss");
+            sliderback2.Value = media1.Position.TotalSeconds;
+        }
+
+        private void downloadButton_Click(object sender, RoutedEventArgs e)
+        {
+           // media1.Source = new Uri(textBox1.Text);
+            OpenFileDialog openDialog = new OpenFileDialog();
+            openDialog.Filter = "Video files (*.MP4, *.AVI, *.MKW, *.WMV)|*.mp4;*.avi;*.mkv; *.wmv"; 
+            if (openDialog.ShowDialog() == true)
+            {
+                media1.Source = new Uri(openDialog.FileName);
+
+            }
+        }
+        private void playButton_Click(object sender, RoutedEventArgs e)
+        {
+            media1.Play();
+            timer.Start();
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            media1.Pause();
+            timer.Stop();
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            media1.Stop();
+            timer.Stop();
+        }
+
+        private void media1_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            slider2.Maximum = media1.NaturalDuration.TimeSpan.TotalSeconds;
+            sliderback2.Maximum = media1.NaturalDuration.TimeSpan.TotalSeconds;
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (media1 != null)
+            {
+                media1.Volume = slider1.Value;
+            }
+        }
+
+        private void slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            media1.Pause();
+            media1.Position = TimeSpan.FromSeconds(slider2.Value);
+            media1.Play();
+        }
+
+        private void FixButton_Click(object sender, RoutedEventArgs e)
+        {
+            int width = media1.NaturalVideoWidth;
+            int height = media1.NaturalVideoHeight;
+            var bitmap = new RenderTargetBitmap(width, height, 96, 96, PixelFormats.Pbgra32);
+            var vb = new VisualBrush(media1);
+            DrawingVisual dv = new DrawingVisual();
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                dc.DrawRectangle(vb, null, new Rect(new Size(width, height)));
+            }
+            bitmap.Render(dv);
+            image.Source = bitmap;
+        }
+
+
+
+    }
+}
